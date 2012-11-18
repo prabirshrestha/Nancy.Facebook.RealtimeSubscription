cea67356df1e725e18c234faacf61ee5f50947f8
Prabir Shrestha <mail@prabir.me>
2012-11-18 01:57:45 -0500

    added FacebookRealtimeSubscriptionExtensions.AsFacebookGetSubscription

---
+++ src/Nancy.Facebook.RealtimeSubscription/Extensions/FacebookRealtimeSubscriptionExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace Nancy.Facebook.RealtimeSubscription.Extensions
+{
+    using System;
+    using Nancy.Responses;
+
+    public static class FacebookRealtimeSubscriptionExtensions
+    {
+        public static Response AsFacebookGetSubscription(this IResponseFormatter responseFormatter, string verifyToken, bool throwException = false)
+        {
+            try
+            {
+                var request = responseFormatter.Context.Request;
+
+                string hubChallenge = request.Query[FacebookClient.SubscriptionHubChallengeKey];
+
+                FacebookClient.VerifyGetSubscription(
+                    request.Query[FacebookClient.SubscriptionHubModeKey],
+                    request.Query[FacebookClient.SubscriptionHubVerifyTokenKey],
+                    hubChallenge,
+                    verifyToken);
+
+                return new TextResponse(hubChallenge);
+            }
+            catch (Exception)
+            {
+                if (throwException)
+                {
+                    throw;
+                }
+
+                return new Response { StatusCode = HttpStatusCode.BadRequest };
+            }
+        }
+    }
+}
