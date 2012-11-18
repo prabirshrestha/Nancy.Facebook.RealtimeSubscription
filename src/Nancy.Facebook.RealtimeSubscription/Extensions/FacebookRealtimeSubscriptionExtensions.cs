2536e3d6cc619e1de0af9dea740b148375f65b8d
Prabir Shrestha <mail@prabir.me>
2012-11-18 02:05:05 -0500

    added AsFacebookPostSubscription extension method

--- src/Nancy.Facebook.RealtimeSubscription/Extensions/FacebookRealtimeSubscriptionExtensions.cs
+++ src/Nancy.Facebook.RealtimeSubscription/Extensions/FacebookRealtimeSubscriptionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿namespace Nancy.Facebook.RealtimeSubscription.Extensions
 {
     using System;
+    using System.IO;
+    using System.Linq;
     using Nancy.Responses;
 
     public static class FacebookRealtimeSubscriptionExtensions
@@ -29,6 +31,34 @@
                 }
 
                 return new Response { StatusCode = HttpStatusCode.BadRequest };
+            }
+        }
+
+        public static object AsFacebookPostSubscription(this IResponseFormatter responseFormatter, string appSecret, Func<object, object> callback, bool throwsException = false)
+        {
+            var request = responseFormatter.Context.Request;
+            var requestBodyString = new StreamReader(request.Body).ReadToEnd();
+
+            try
+            {
+                var result = FacebookClient.VerifyPostSubscription(
+                    request.Headers[FacebookClient.SubscriptionXHubSigntureRequestHeaderKey].FirstOrDefault(),
+                    requestBodyString,
+                    null,
+                    appSecret,
+                    null);
+
+                if (callback != null)
+                {
+                    return callback(result);
+                }
+
+                return HttpStatusCode.OK;
+            }
+            catch (Exception)
+            {
+                if (throwsException) throw;
+                return new Response { StatusCode = HttpStatusCode.NotFound };
             }
         }
     }
